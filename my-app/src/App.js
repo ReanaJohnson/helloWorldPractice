6fa868ee03fb278f4b7142cae01d8c5d2bb1153d
ReanaJohnson <Reana.Johnson@cdpipelinedevshop.com>
2022-07-11 16:32:07 +0000

    First commit

---
+++ my-app/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+         Reana Johnson
+        </p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Button variant="contained">Hello World</Button>;
+      </header>
+    </div>
+  );
+}
+
+export default App;
